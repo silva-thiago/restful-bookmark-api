443996e7cf3e57763cdae7c5bbab9fc5957be861
T. J. Silva <thiago.silva@imd.ufrn.br>
2023-06-17 14:44:20 -0300

    feat: add auth, bookmark and user modules
    The AppModule now imports these new modules. Additionally, creates the AuthController and AuthService files for handling authentication logic. Finally, it also creates empty BookmarkModule and UserModule files that will be used in future development.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,18 @@
 import { Module } from '@nestjs/common'
 import { DevtoolsModule } from '@nestjs/devtools-integration'
 
-import { AppController } from './app.controller'
-import { AppService } from './app.service'
+import { AuthModule } from './auth/auth.module'
+import { BookmarkModule } from './bookmark/BookmarkModule'
+import { UserModule } from './user/UserModule'
 
 @Module({
 	imports: [
 		DevtoolsModule.register({
 			http: process.env.NODE_ENV !== 'production'
-		})
+		}),
+		AuthModule,
+		BookmarkModule,
+		UserModule
 	],
 })
 export class AppModule {}
