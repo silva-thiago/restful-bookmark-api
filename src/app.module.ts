d642588a1b30b5b93c36165e458917763ab6431e
T. J. Silva <thiago.silva@imd.ufrn.br>
2023-06-17 13:54:32 -0300

    feat: add devtools-integration and snapshot options
    Adds the "@nestjs/devtools-integration" dependency to package.json, registers DevtoolsModule in app.module.ts, and sets the "snapshot" option in main.ts. The DevtoolsModule is configured to only run when not in production mode.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,14 @@
 import { Module } from '@nestjs/common'
+import { DevtoolsModule } from '@nestjs/devtools-integration'
 
 import { AppController } from './app.controller'
 import { AppService } from './app.service'
 
 @Module({
-	imports: [],
-	controllers: [AppController],
-	providers: [AppService],
+	imports: [
+		DevtoolsModule.register({
+			http: process.env.NODE_ENV !== 'production'
+		})
+	],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,9 @@ import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
 
 const bootstrap = async () => {
-	const app = await NestFactory.create(AppModule)
+	const app = await NestFactory.create(AppModule, {
+		snapshot: true
+	})
 	await app.listen(3000)
 }
 bootstrap()
