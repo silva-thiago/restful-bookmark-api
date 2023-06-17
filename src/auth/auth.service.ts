bd5e048dbee86d9e52967520ac081574cb10a6fd
T. J. Silva <thiago.silva@imd.ufrn.br>
2023-06-17 14:45:18 -0300

    feat: add signup and signin endpoints to auth controller
    Adds two new endpoints, /signup and /signin, to the AuthController. These endpoints call the corresponding methods in AuthService which return a message indicating that the user has signed up or signed in.

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,12 @@
 import { Injectable } from "@nestjs/common"
 
 @Injectable({})
-export class AuthService {}
+export class AuthService {
+  signup() {
+    return { message: 'You have signed up' }
+  }
+
+  signin() {
+    return { message: 'You have signed in' }
+  }
+}
